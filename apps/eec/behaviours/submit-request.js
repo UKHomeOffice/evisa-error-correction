2218d04a83d819a46e4ccd07b49c18ff44e570d5
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-09-18 17:25:52 +0100

    EEC-6 Add basic behaviour and config update

---
+++ apps/eec/behaviours/submit-request.js
@@ -0,0 +1,61 @@
+const config = require('../../../config');
+const logger = require('hof/lib/logger')({ env: config.env });
+const {
+  notifyApiKey,
+  caseworkerEmail,
+  userConfirmationTemplateId,
+  businessConfirmationTemplateId,
+  replyToId
+} = config.govukNotify;
+
+const { getLabel } = require('../../../utils');
+
+const NotifyClient = require('notifications-node-client').NotifyClient;
+const Notify = new NotifyClient(notifyApiKey);
+
+module.exports = superclass => class extends superclass {
+  async saveValues(req, res, next) {
+
+    const baseEmailProps = {
+      full_name: req.sessionModel.get('requestor-full-name')
+    };
+
+    const caseworkerEmailProps = {
+      ...baseEmailProps
+    }
+
+    try {
+      await Notify.sendEmail(
+        businessConfirmationTemplateId,
+        caseworkerEmail,
+        {
+          personalisation: caseworkerEmailProps,
+          emailReplyToId: replyToId
+        }
+      );
+      logger.info('EEC request email sent successfully');
+
+      const requestorEmail = req.sessionModel.get('requestor-email')
+      if (requestorEmail) {
+        await Notify.sendEmail(
+          userConfirmationTemplateId,
+          requestorEmail,
+          {
+            personalisation: baseEmailProps,
+            emailReplyToId: replyToId
+          }
+        );
+        logger.info('EEC requestor acknowledgement email sent successfully');
+      }
+    } catch (error) {
+      const errorDetails = error.response?.data ? `Cause: ${JSON.stringify(error.response.data)}` : '';
+      const errorCode = error.code ? `${error.code} -` : '';
+      const errorMessage = `${errorCode} ${error.message}; ${errorDetails}`;
+
+      logger.error(`Failed to send EEC request email: ${errorMessage}`);
+      return next(error);
+    }
+
+    return super.saveValues(req, res, next);
+  }
+};
--- config.js
+++ config.js
@@ -15,7 +15,8 @@ module.exports = {
     notifyApiKey: process.env.NOTIFY_KEY,
     caseworkerEmail: process.env.CASEWORKER_EMAIL,
     userConfirmationTemplateId: process.env.USER_CONFIRMATION_TEMPLATE_ID,
-    businessConfirmationTemplateId: process.env.BUSINESS_CONFIRMATION_TEMPLATE_ID
+    businessConfirmationTemplateId: process.env.BUSINESS_CONFIRMATION_TEMPLATE_ID,
+    replyToId: process.env.EMAIL_REPLY_TO_ID
   },
   redis: {
     port: process.env.REDIS_PORT || '6379',
