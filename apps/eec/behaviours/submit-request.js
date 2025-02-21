39b38fb10bf2949a86bc7b44cb2088a118f7aaa8
Toby Schaeffer <toby.schaeffer@digital.homeoffice.gov.uk>
2025-02-21 16:43:54 +0300

    - added property required by email template (submit-request.js)
    - updated unit tests (submit-request.test.js)

--- apps/eec/behaviours/submit-request.js
+++ apps/eec/behaviours/submit-request.js
@@ -55,6 +55,7 @@ module.exports = superclass => class extends superclass {
         in_uk: getLabel('in-uk', req.sessionModel.get('in-uk')),
         is_not_accessing_evisa: req.sessionModel.get('accessing-evisa') === 'no' ? 'yes' : 'no',
         accessing_evisa: getLabel('accessing-evisa', req.sessionModel.get('accessing-evisa')),
+        accessing_evisa_yesOrNo: req.sessionModel.get('accessing-evisa'),
         full_name: req.sessionModel.get('requestor-full-name'),
         date_of_birth: formatDate(req.sessionModel.get('requestor-dob')),
         nationality: req.sessionModel.get('requestor-nationality'),
