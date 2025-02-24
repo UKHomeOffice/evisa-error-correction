f70e0f3c43e376070467a49231f72ba20d570a8b
Toby Schaeffer <toby.schaeffer@digital.homeoffice.gov.uk>
2025-02-24 15:03:05 +0300

    EEC-58: Question text update (requires update to hof-services-config) (#27)
    * EEC-58: Question text update
    
    - updated text for /viewing-evisa (fields.json)
    
    * - updated path in line with text changes
    (index.js)
    
    - refs for email template date (submit-request.js)
    
    - property for fields data (fields/index.js)
    
    - step for summary data sections;
      renamed method for checking submitted data
    (summary-data-sections.js)
    
    - fields for route (fields.json)
    
    - ref for page label (pages.json)
    
    - ref for validation (validation.json)
    
    - properties for testing (submit-request.test.js)
    
    * - added property required by email template (submit-request.js)
    
    - updated unit tests (submit-request.test.js)

--- test/unit/submit-request.test.js
+++ test/unit/submit-request.test.js
@@ -51,7 +51,7 @@ describe('submit-feedback behaviour', () => {
       req.sessionModel = new Model({
         problem: ['problem-photo', 'problem-nin'],
         'in-uk': 'yes',
-        'viewing-evisa': 'no',
+        'accessing-evisa': 'no',
         'detail-photo': 'photo bad',
         'detail-nin': 'QQ123456A',
         'requestor-full-name': 'test user',
@@ -102,8 +102,9 @@ describe('submit-feedback behaviour', () => {
       emailProps = {
         personalisation: {
           in_uk: 'Yes',
-          is_not_viewing_evisa: 'yes',
-          viewing_evisa: 'No, the problem is something else',
+          is_not_accessing_evisa: 'yes',
+          accessing_evisa: 'No, the problem is something else',
+          accessing_evisa_yesOrNo: 'no',
           full_name: 'test user',
           date_of_birth: '14/08/1987',
           nationality: 'France',
@@ -133,8 +134,9 @@ describe('submit-feedback behaviour', () => {
       emailProps = {
         personalisation: {
           in_uk: 'Yes',
-          is_not_viewing_evisa: 'yes',
-          viewing_evisa: 'No, the problem is something else',
+          is_not_accessing_evisa: 'yes',
+          accessing_evisa: 'No, the problem is something else',
+          accessing_evisa_yesOrNo: 'no',
           full_name: 'test user',
           date_of_birth: '14/08/1987',
           nationality: 'France',
@@ -161,8 +163,9 @@ describe('submit-feedback behaviour', () => {
       emailProps = {
         personalisation: {
           in_uk: 'Yes',
-          is_not_viewing_evisa: 'yes',
-          viewing_evisa: 'No, the problem is something else',
+          is_not_accessing_evisa: 'yes',
+          accessing_evisa: 'No, the problem is something else',
+          accessing_evisa_yesOrNo: 'no',
           full_name: 'test user',
           date_of_birth: '14/08/1987',
           nationality: 'France',
