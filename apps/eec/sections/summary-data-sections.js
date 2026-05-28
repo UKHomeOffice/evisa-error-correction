838f455c882cb8b51565625298f04976461954e9
anjurajanHO <206862292+anjurajanHO@users.noreply.github.com>
2026-05-28 10:57:00 +0100

    EEC-165: Add new page to enter immigration status problems. (#72)
    * remove unwanted codes
    
    * rename fields and apply CYA page changes.

--- apps/eec/sections/summary-data-sections.js
+++ apps/eec/sections/summary-data-sections.js
@@ -52,8 +52,8 @@ module.exports = {
         field: 'correct-nationality'
       },
       {
-        step: '/problem',
-        field: 'detail-status'
+        step: '/problem-immigration-status',
+        field: 'problem-immigration-status'
       },
       {
         step: '/problem',
